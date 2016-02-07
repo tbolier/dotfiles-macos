6dd29d3c0f07a51ee20dccbc844ffdf5ee3af51e
Timon Bolier <t.bolier@coolblue.nl>
2016-02-07 12:55:27 +0100

    [BOYSCOUT] Add slack to brew install

--- brew.sh
+++ brew.sh
@@ -76,6 +76,7 @@ brew cask install phpstorm
 brew cask install puppet
 brew cask install real-vnc
 brew cask install sequel-pro
+brew cask install slack
 brew cask install sourcetree
 brew cask install spectacle
 brew cask install spotify
