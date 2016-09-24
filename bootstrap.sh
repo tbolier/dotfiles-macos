1d4ded6ee733d48041339fa18db37c4946c77f79
Timon Bolier <timonb@gmail.com>
2016-09-24 10:28:37 +0200

    Ignore .idea folder in the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,7 @@ git pull origin master;
 
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".idea*" -avh --no-perms . ~;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -20,3 +19,5 @@ else
 	fi;
 fi;
 unset doIt;
+
+source ~/.bash_profile;
